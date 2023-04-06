7b9ae4f9c35ed6a96396add4d2de73d1c2eefc9d
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-04-06 08:56:55 +0530

    Update bot.py

--- main/plugins/bot.py
+++ main/plugins/bot.py
@@ -1,11 +1,20 @@
 #ChauhanMahesh/Vasusen/COL/DroneBots
 
+from telethon import events
 import asyncio
+from .. import Drone
 
-async def clone(bot, source, destination):
-    async for msg in bot.iter_messages(int(source), reverse=True):
+async def clone(bot, source, destination, msg_id, media_only=True):
+    async for msg in bot.iter_messages(int(source), min_id=int(msg_id), reverse=True):
         try:
             await asyncio.sleep(3)
+            if media_only=False:
+                try:
+                    await bot.send_message(destination, msg)
+                    return
+                except Exception as exc:
+                    print(str(exc))
+                    return
             if msg.poll:
                 return
             if msg.photo:
@@ -31,3 +40,14 @@ async def clone(bot, source, destination):
         except Exception as exc:
             print(str(exc))
 
+@Drone.on(events.NewMessage(outgoing=True, pattern="^/run (.*)"))
+async def run(event):
+    c = (event.text).split(" ")
+    if not len(c) == 5 :
+        await event.edit("Incorrect format")
+        return
+    if c[4] == "True":
+        media=True
+    else:
+        media=False 
+    await clone(event.client, int(c[1]), int(c[2]), int(c[3]), media_only=media)
