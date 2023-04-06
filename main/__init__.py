6e62b14e49c4c3697854d77a1c6480b8a47aa6e6
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-04-06 09:04:42 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -4,6 +4,7 @@ from telethon.sessions import StringSession
 from telethon import TelegramClient
 import logging
 import time
+import sys
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -14,3 +15,9 @@ API_HASH =
 SESSION = 
 
 Drone = TelegramClient(StringSession(SESSION) , API_ID, API_HASH)
+
+try:
+    Drone.start()
+except Exception:
+    print("Userbot Error ! Have you added a SESSION in deploying??")
+    sys.exit()
